09bf2dbb9a06622b90d2c54a9d90a7f27f681124
AlexanderAsaturyan <alexanderasaturyan@gmail.com>
2023-09-06 00:28:44 +0400

    Added functionality of looking around.

---
+++ Assets/Scripts/Look/LookAround.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class LookAround : MonoBehaviour
+{
+    [SerializeField] private Transform playerBody;
+
+    private float mouseSensitivity = 100;
+    private float xRotation;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
